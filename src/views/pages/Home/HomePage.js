573163e9274575521edafb40b87e79bd24a02d96
Ragini sahu <raginisahu@Raginis-MacBook-Pro.local>
2021-11-30 22:23:30 +0530

    commented home page

--- src/views/pages/Home/HomePage.js
+++ src/views/pages/Home/HomePage.js
@@ -25,7 +25,7 @@ return (
           Games have begun
         </h2>
         <Link to="/games" className="button button__cta">Launch Game</Link>
-<div className="intro intro-content">
+        {/* <div className="intro intro-content">
           <Link to="/gameTwo" className="globalBtn">Snack Game</Link>
           <br></br>
           <Link to="/gameThree" className="globalBtn"> Breakout Game</Link>
@@ -36,7 +36,7 @@ return (
           <br></br>
           <Link to="/gameSix" className="globalBtn">T-Rex Game</Link>
           <br></br>
-         </div>
+         </div> */}
       </div>
 
       <div className="site-teaser default-spacing">
